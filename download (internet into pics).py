a3112a0139000b0c867e0c2603c34ccfa282bfcd
Brambor <tondadrd@seznam.cz>
2016-01-13 01:48:16 +0100

    used threading, but it doesn't speed up!
    now each request is it's own thread, why does it not speed up the thing?

--- download (internet into pics).py
+++ download (internet into pics).py
@@ -1,6 +1,7 @@
 from urllib.request import Request, urlopen
 import time
 import os
+import _thread
 
 def myrequest(url):
 	worked = False
@@ -25,7 +26,7 @@ if not os.path.exists(allpath):
 path_urls = "{}/{}".format(path, "urls.txt")
 
 if os.path.exists(path_urls):
-	check = input("Wana check chapters? (write something for YES, enter for NO: ")
+	check = input("Wana check chapters? (write something for YES, enter for NO): ")
 else:
 	check = True
 if check != "":
@@ -47,32 +48,51 @@ if check != "":
 
 	urls_eps = []
 	if os.path.exists(path_urls):
-		urlfile = open(path_urls, "r")
-		ufr = urlfile.read()
-		urls_eps = ufr.split("\n") if ufr != "" else []
-		urlfile.close()
+		with open(path_urls, "r") as urlfile:
+			urls_eps = urlfile.read().split("\n")
+			if urls_eps[0] == "":
+				del urls_eps[0]
+				with open(path_urls, "w") as urlfile:
+					urlfile.write("\n".join(urls_eps))
+
+
 
 	urls = [ue.split()[0] for ue in urls_eps]
-	chaps_to_search = []
-	for ch in chapurl_sorted:
-		if ch not in urls:
-			chaps_to_search.append(ch)
+	chaps_to_search = chapurl_sorted
 
-	for ch in chaps_to_search:
+	for u in urls:
+		if u in chapurl_sorted:
+			del chapurl_sorted[chapurl_sorted.index(u)]
+
+
+	def thrue_thread_download_pages(ch, path_urls):
+		global finished
 		ted = time.time()
 		currentchap = ch.split("/")[-1]
 		webpage_read = myrequest(ch).decode("utf-8")
 		allepurl = webpage_read.split('onchange="change_page(this)">')[1].split("</select>")[0]
 		there_are_x_ep = len(allepurl.split("{}/".format(currentchap))) - 1
-		print("in chap {}\tare {} ep\t{}".format(float(currentchap[1:]) if "." in currentchap else int(currentchap[1:]), there_are_x_ep, time.time() - ted))
-		urls_eps.append("{} {}".format(ch, there_are_x_ep))
+		#print("in chap {}\tare {} ep\t{}".format(float(currentchap[1:]) if "." in currentchap else int(currentchap[1:]), there_are_x_ep, time.time() - ted))
+		urls_eps.append("\n{} {}".format(ch, there_are_x_ep))
+		addhere = "\n{} {}".format(ch, there_are_x_ep)
 
-		urlfile = open(path_urls, "w")
-		urlfile.write("\n".join(urls_eps))
-		urlfile.close()
 
-	print("It took {} secs to check chapters".format(time.time() - start))
+		with open(path_urls, "a") as urlfile:
+			urlfile.write(addhere)
+		finished += 1
+	finished = 0
+	for ch in chaps_to_search:
+		_thread.start_new_thread(thrue_thread_download_pages, (ch, path_urls))
 
+	#print("It took {} secs to check chapters".format(time.time() - start))
+	
+	checker = -1
+	goal = len(chaps_to_search)
+	while finished != goal:
+		if finished != checker:
+			checker = finished
+			print("{} of {} checked".format(finished, goal))
+		time.sleep(0.1)
 
 
 print("Downloading pics...")
@@ -85,6 +105,26 @@ urlfile.close()
 
 downloaded = [f.split(".")[0] for f in os.listdir("{}/pics".format(path))] #"E:/MGX/MGX pics"
 celkem = 0
+
+goal = 0
+finished = 0
+lock = _thread.allocate_lock()
+def thrue_thread_download_pics(path, url, ep, name):
+	lock.acquire()
+	global goal
+	goal += 1
+	lock.release()
+	webpage_read = myrequest("{}/{}.html".format(url, ep))
+	url_to_pic = webpage_read.decode("utf-8").split('" onerror="')[0].split('<img src="')[-1]
+
+	pic = myrequest(url_to_pic)
+
+	myfile = open("{}/pics/{}.jpg".format(path, name), "wb")
+	myfile.write(pic)
+	myfile.close()
+	global finished
+	finished += 1
+
 
 for url_ep in urls_eps:
 
@@ -100,21 +140,20 @@ for url_ep in urls_eps:
 		ted = time.time()
 		name = "{}{}".format(chap, "{}{}".format((2 - len(str(ep))) * "0", ep))
 		if name in downloaded:
-			print("skipped {}".format(name))
+			#print("skipped {}".format(name))
 			continue
 
-		webpage_read = myrequest("{}/{}.html".format(url, ep))
-		url_to_pic = webpage_read.decode("utf-8").split('" onerror="')[0].split('<img src="')[-1]
+		###
+		_thread.start_new_thread(thrue_thread_download_pics, (path, url, ep, name))
+		###
+		#print("strana {}\tza {}\tsekund".format(name, time.time() - ted))
 
-		pic = myrequest(url_to_pic)
-
-		myfile = open("{}/pics/{}.jpg".format(path, name), "wb")
-		myfile.write(pic)
-		myfile.close()
-		celkem += 1
-
-		print("strana {}\tza {}\tsekund".format(name, time.time() - ted))
-		#myfile = open("{}\{}.jpg".format("{}\\{}".format(path, "MGX pics"), petinazev), "wb")
+checker = -1
+while finished != goal:
+	if finished != checker:
+		checker = finished
+		print("{} of {} downloaded".format(finished, goal))
+	time.sleep(0.1)
 
 print("{} stranek\tza {}\tsekund".format(celkem, time.time() - start2))
 
